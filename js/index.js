90ffa596098ff46b36ebd690504673c113b8ece8
Joshix <joshix.dev@mailbox.org>
2021-03-28 22:59:15 +0200

    fix the code like with the plugin

--- js/index.js
+++ js/index.js
@@ -1,40 +1,28 @@
 let json  = {
     "aggressiv": "attraktiv",
-    "aggressive": "attractive",
 
     "amüsant": "relevant",
     "amüsanz": "relevanz",
-    "amusing": "relevant",
-    "amusement": "relevance",
 
     "ministerium": "mysterium",
     "ministerien": "mysterien",
-    "ministry": "mystery",
-    "ministries": "mystries",
 
     "bundestag": "schützenverein",
-    "congress": "the nra",
 
     "ironisch": "erotisch",
+    "ironien": "erotiken",
     "ironie": "erotik",
     "ironiker": "erotiker",
-    "ironic": "erotic",
-    "irony": "erotica",
-    "ironist": "eroticist",
 
     "problem": "ekzem",
-    "problem": "eczema",
 
     "kritisch": "kryptisch",
     "kritik": "kryptik",
-    "critical": "cryptic",
-    "criticism": "cryptics",
 
     "provozier": "produzier",
-    "provocate": "produce",
 
     "arbeitnehmer": "arbeitgeber",
-    "employee": "employer",
+    "arbeitsnehmer": "arbeitsgeber"
 };
 
 let keys = Object.keys(json);
@@ -43,6 +31,8 @@ for (let i = 0; i < keys.length; i++) {
     words[json[keys[i]]] = keys[i]; //value as key with old key as new value
 }
 keys = Object.keys(words);
+// sort from long to short to replace the longer once with higher priority
+keys.sort((a,b) => b.length - a.length);
 
 function getUrlVars() {
     const vars = {};
@@ -62,52 +52,84 @@ function getUrlParam(parameter) {
 
 document.getElementById("text_input").value = getUrlParam("text");
 
+function isText(val) {
+    if (typeof val !== "string") return false;
+    if (val.length === 0) return false;
+    return val.replace(noTextRegex, "").length === 0;
+}
+
+function arrIsEmpty(arr) {
+    if (!Array.isArray(arr)) return true;
+    if (arr.length === 0) return true;
+    for (const arrElement of arr) {
+        if (arrElement.length > 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const noTextRegex = /[^a-zA-ZÄÖÜäöü]+/gm;
+const textRegex = /[a-zA-ZÄÖÜäöü]+/gm;
 function replaceText(input) {
     if(input.length === 0) return "";
 
-    const text = input.split(/[^a-zA-ZÄÖÜäöü]+/); // everything that isn't word
-    const notText = input.split(/[a-zA-ZÄÖÜäöü]+/);  //everything that is word
+    const text = input.split(noTextRegex); // everything that isn't word
 
-
-    const startsWithText = text[0].length > 0;
-
-    function getNextTextPart(index, replacedText) {
-        let str = "";
-        if(startsWithText) {
-            str += replacedText;
-            if(index + 1 < notText.length) str += notText[index + 1];
-        } else {
-            if(index - 1 < notText.length) str += notText[index - 1];
-            str += replacedText;
-        }
-        return str;
+    if (arrIsEmpty(text)) {
+        return input;
     }
 
+    const notText = input.split(textRegex);  //everything that is word
+
+    let noTextIndex = 0;
+    let textIndex = 0;
     let out = "";
-    for (let i = startsWithText ? 0 : 1; i < text.length; i++) {
-        if(text[i].length > 0) {
-            let replacement = text[i].toLowerCase();
+
+    if (!isText(input[0])) { // if not starts with text
+        out = notText[0];
+        noTextIndex++;
+
+        if (text[0].length === 0) {
+            textIndex = 1;
+        }
+    }
+
+    for (; textIndex < text.length; textIndex++) {
+        if(text[textIndex].length > 0) {
+            let replacement = text[textIndex].toLowerCase();
+            let replaced = false;
 
             for (let j = 0; j < keys.length; j++) {
                 if (replacement.indexOf(keys[j]) !== -1) {
+                    replaced = true;
                     replacement = replacement.replace(keys[j], words[keys[j]]);
                     break;
                 }
             }
 
-            if (replacement !== text[i] && replacement .length > 0) {
-                if (text[i].toUpperCase() === text[i]) { //checks if string is uppercase
+            if(!replaced) {
+                replacement = text[textIndex];
+            } else if (replacement .length > 0) {
+                if (text[textIndex].toUpperCase() === text[textIndex]) { //checks if string is uppercase
                     replacement = replacement.toUpperCase();
                 } else {
-                    const firstLetterOfText = text[i].charAt(0); //first letter
+                    const firstLetterOfText = text[textIndex].charAt(0);
                     if (firstLetterOfText.toUpperCase() === firstLetterOfText) { //checks if first letter is uppercase
                         replacement = replacement.charAt(0).toUpperCase() + replacement.slice(1); //sets first letter uppercase, to match case with the original word
                     }
                 }
             }
 
-            out += getNextTextPart(i, replacement);
+            out += replacement;
         }
+        if (noTextIndex < notText.length) {
+            out += notText[noTextIndex];
+            noTextIndex += 1;
+        }
+    }
+    for (; noTextIndex < notText.length; noTextIndex++) {
+        out += notText[noTextIndex];
     }
     return out;
 }
